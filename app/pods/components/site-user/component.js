e152504c58a7db5342832e6faaef76268fa1c0df
jgthms <mschinis@gmail.com>
2015-09-25 22:05:56 +0100

    feat(site-user): Adds user component

---
+++ app/pods/components/site-user/component.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+    // Required input parameters
+    user: {},
+    callAction: null,
+    
+    actions:{
+        callUser(){
+
+        }
+    }
+});
