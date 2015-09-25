668314981ab342bf265f25de3c1b2f7619558c28
jgthms <bbxdesign@gmail.com>
2015-09-25 21:53:01 +0100

    feat(dashboard): List all users

--- app/pods/dashboard/route.js
+++ app/pods/dashboard/route.js
@@ -1,4 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    model(){
+        let users = [
+            {
+                id: 1,
+                full_name: 'Michael Schinis',
+                group: 1
+            },
+            {
+                id: 2,
+                full_name: 'Mattia Asti',
+                group: 1
+            }
+        ];
+
+        return {
+            users
+        }
+    }
 });
