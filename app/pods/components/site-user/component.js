7d70295ae9ddc09851087ed18aff748e7faf416c
jgthms <mschinis@gmail.com>
2015-09-25 22:35:25 +0100

    feat(site-user): Display name and call button

--- app/pods/components/site-user/component.js
+++ app/pods/components/site-user/component.js
@@ -4,10 +4,10 @@ export default Ember.Component.extend({
     // Required input parameters
     user: {},
     callAction: null,
-    
+    // Actions
     actions:{
         callUser(){
-
+            this.sendAction('callUser',this.get('user'));
         }
     }
 });
